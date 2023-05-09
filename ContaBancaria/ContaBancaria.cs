c5f078d83af874b9c1d79712e3c7a0b65c12053b
Kndofc <kauan.lopess@outlook.com>
2023-05-08 22:39:56 -0300

    first commit

---
+++ ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class ContaBancaria
+{
+    private decimal saldo;
+
+    public ContaBancaria(decimal saldoInicial)
+    {
+        saldo = saldoInicial;
+    }
+
+    public void Depositar(decimal valor)
+    {
+        saldo += valor;
+        Console.WriteLine($"Depósito realizado. Saldo atual: {saldo:C}");
+    }
+
+    public void Sacar(decimal valor)
+    {
+        if (saldo >= valor)
+        {
+            saldo -= valor;
+            Console.WriteLine($"Saque realizado. Saldo atual: {saldo:C}");
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente para saque.");
+        }
+    }
+
+    public void VerificarSaldo()
+    {
+        Console.WriteLine($"Saldo atual: {saldo:C}");
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira o saldo inicial: ");
+        decimal saldoInicial = decimal.Parse(Console.ReadLine());
+
+        ContaBancaria conta = new ContaBancaria(saldoInicial);
+
+        while (true)
+        {
+            Console.WriteLine("\nEscolha uma operação:");
+            Console.WriteLine("1. Depositar");
+            Console.WriteLine("2. Sacar");
+            Console.WriteLine("3. Verificar saldo");
+            Console.WriteLine("4. Sair");
+
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.Write("Insira o valor a depositar: ");
+                    decimal valorDeposito = decimal.Parse(Console.ReadLine());
+                    conta.Depositar(valorDeposito);
+                    break;
+
+                case 2:
+                    Console.Write("Insira o valor a sacar: ");
+                    decimal valorSaque = decimal.Parse(Console.ReadLine());
+                    conta.Sacar(valorSaque);
+                    break;
+
+                case 3:
+                    conta.VerificarSaldo();
+                    break;
+
+                case 4:
+                    Console.WriteLine("Saindo...");
+                    return;
+
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
